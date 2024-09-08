2daae81e2972601bc60507d35e5069f20f32ecd6
KODOKYUN <hwasungnj3@gmail.com>
2024-09-08 11:44:20 +0900

    240908 AnimalFace 작업 중

--- app/src/main/java/com/project/animalface_app/kdkapp/network/MyApplication.kt
+++ app/src/main/java/com/project/animalface_app/kdkapp/network/MyApplication.kt
@@ -4,10 +4,11 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import com.project.animalface_app.kdkapp.network.ImageUploadApi
+import com.project.animalface_app.kdkapp.network.INetworkService
 
 object MyApplication {
     private val BASE_URL = "http://10.100.201.42:8080/" // localhost 대신 사용
+    private lateinit var apiService: INetworkService
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -17,15 +18,17 @@ object MyApplication {
         .addInterceptor(loggingInterceptor)
         .build()
 
+    fun getApiService(): INetworkService {
+        return apiService
+    }
 
-
-    val instance: ImageUploadApi by lazy {
+    val instance: INetworkService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        retrofit.create(ImageUploadApi::class.java)
+        retrofit.create(INetworkService::class.java)
     }
 }
