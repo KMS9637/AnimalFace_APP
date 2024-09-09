9069335079fd0f31d683b072456d61206a9717e4
KODOKYUN <hwasungnj3@gmail.com>
2024-09-09 18:44:31 +0900

    240909 전체 적인 정리 및 주석 제거

--- app/src/main/java/com/project/animalface_app/kmsapp/dto/UserDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.project.animalface_app.kmsapp.dto
-
-data class UserDTO(
-    val memberName: String,
-    val memberId: String,
-    val memberPw: String
-)
--- app/src/main/java/com/project/animalface_app/ohjapp/ksyAPI/CreateGame.kt
+++
@@ -1,8 +0,0 @@
-package com.project.animalface_app.ohjapp.ksyAPI
-
-data class CreateGame(val createGameNo: Long,
-                      val createGameName: String,
-                      val createQuestion: String,
-                      val createAnswer: String,
-                      val createResult: String
-)
